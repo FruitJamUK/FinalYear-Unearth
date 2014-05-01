279e62dd1cf0cdd8a215df9cbd8c218126e0fb35
FruitJamUK <FruitJamUK@gmail.com>
2014-05-01 14:34:21 +0100

    Added Javadoc comments

--- Unearth/app/models/Restaurant.java
+++ Unearth/app/models/Restaurant.java
@@ -1,16 +1,26 @@
 package models;
 
 import com.fasterxml.jackson.databind.JsonNode;
-
+/**
+ * 
+ * @author David Sharp <dps15@uni.brighton.ac.uk>
+ * Containing appropriate getters
+ */
 public class Restaurant {
 	String formAdd;
 	String lat;
 	String lon;
 	String name;
-	String rating;
+	/**
+	 * Placeholder value
+	 */
+	String rating="4.5";
 	
-	
-	//public static void setLatLon(String LatLon){}
+	/**
+	 * The readJson class takes a subset of Google served JSON (representing a single restaurant)
+	 * and attributes its contents to the local variables within
+	 * @param json JsonNode representing an individual restaurant of the JSON returned by Google
+	 */
 	public void readJson(JsonNode json)
 	{
 		JsonNode LatLon;
@@ -22,5 +32,14 @@ public class Restaurant {
 		name=json.get("name").asText();
 		rating=json.get("rating").asText();
 	}
-	public static String buildHtml(){return null;}
+	public String getAddress(){return formAdd;}
+	public String getLat(){return lat;}
+	public String getLon(){return lon;}
+	public String getName(){return name;}
+	public String getRating(){return rating;}
+	
+	/**
+	 * @return A test string to debug the contents of Restaurant
+	 */
+	public String testString(){return formAdd+lat+lon+name+rating;}
 }
