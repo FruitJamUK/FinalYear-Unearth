4398a343592b0e337ba61e0f161635838056b860
FruitJamUK <FruitJamUK@gmail.com>
2014-05-03 21:01:08 +0100

    Created Integration test

--- Unearth/test/IntegrationTest.java
+++ Unearth/test/IntegrationTest.java
@@ -17,7 +17,9 @@ public class IntegrationTest {
         running(testServer(3333, fakeApplication(inMemoryDatabase())), HTMLUNIT, new Callback<TestBrowser>() {
             public void invoke(TestBrowser browser) {
                 browser.goTo("http://localhost:3333");
-                assertThat(browser.pageSource()).contains("Hello from Java");
+                browser.fill("input[id='query']").with("Brighton");
+                browser.submit("button[id='querySubmit']");
+                assertThat(browser.$("#accordion")).isNotEmpty();
             }
         });
     }
